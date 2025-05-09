4402b3e0727f6a1e5708aed9a8d0f2aa274beae7
ELMcode <113478465+ELMcode@users.noreply.github.com>
2025-05-09 10:13:14 +0200

    Revert "feat/docker-deploy to develop"

--- server/search-suggestion-service/src/main/java/com/ut/ecommerce/searchsuggestionservice/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.ut.ecommerce.searchsuggestionservice.utils;
-
-public class Constants {
-    public static final int MAX_SUGGESTIONS = 10;
-    public static final String DEFAULT_LANGUAGE = "en";
-}
